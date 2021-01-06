1a7ecefb7a9048f10631222525482b97d34b7a54
mholzer85 <mholzer85@gmail.com>
2021-01-06 13:48:05 +0100

    first draft view of the game (stats for player)

--- SuperAdventure.cs
+++ SuperAdventure.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
+        private Player _player;
+
         public SuperAdventure()
         {
             InitializeComponent();
+
+            _player = new Player();
+
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
     }
 }
